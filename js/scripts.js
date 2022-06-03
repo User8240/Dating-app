8b4f9ae312659043646a616f67560c914831176d
ryandefea <ryan.defea@gmail.com>
2022-06-02 21:56:30 -0700

    add final function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
+  
   $("form#matchmaking").submit(function(event) {
   
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
+    
 
   if (age < 30 && gender === 'male') {
     $("#match").text("Miley Cyrus"); 
   } else if (age > 30 && gender === 'male') {
-    console.log(age);
     $("#match").text("Emma Stone");
   } else if (age < 30 && gender === 'female') {
-    $("#match").text("Johnny Depp");
+     $("#match").text("Johnny Depp");
   } else /*(age > 30 && gender === 'female')*/ {
     $("#match").text("Patrick Osten");
   }
-
-  //$(".match").click(function() {
-  //  $(".word-showing").toggle();
-  //  $(".word-hidden").toggle();
-
   event.preventDefault();
   });
+  $("#hide-match").click(function(event) {
+    $("#match").text("");
+    $("#age").val("");
+  })
 });
